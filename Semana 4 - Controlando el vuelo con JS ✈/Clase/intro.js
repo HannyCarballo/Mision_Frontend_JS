571367ee74862216abe9d2293a0b3e2717fba2bb
Hanny Carballo <launchx00194@innovaccion.mc>
2022-07-07 10:23:59 -0500

    Strings

--- Semana 4 - Controlando el vuelo con JS ✈/Clase/intro.js
+++ Semana 4 - Controlando el vuelo con JS ✈/Clase/intro.js
@@ -3,3 +3,10 @@ numero1 = 4;
 var numero2;
 numero2 = 2;
 console.log(numero1);
+
+var frase1;
+frase1 = "Ejemplo comillas dobles";
+var frase2;
+frase2 = 'Ejemplo comillas simples';
+var frase3;
+frase3 = `Ejemplo comillas invertidas`;
